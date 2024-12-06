d3cc456840d40592febaff8ef9afd746a357ed6c
bahar <88434002+bahar-dc@users.noreply.github.com>
2024-12-06 10:01:07 +0500

    github pages

--- wp-content/uploads/simply-static/temp-files/simply-static-1-1733265853/wp-content/themes/elite-design-pro/templates/js/team.js
+++
@@ -1,9 +0,0 @@
-	jQuery(document).ready(function (jQuery) {
-	jQuery(".sm-inner").magnificPopup({
-		type: "inline",
-		midClick: true,
-		mainClass: "mfp-fade",
-		class: ".team-popup",
-		midClick: true,
-	});
-	})
